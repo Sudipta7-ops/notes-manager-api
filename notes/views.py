4a5961c91d183b143fd2bc6d444d613053919e46
Sudipta Barik <bariksudipta1997@gmail.com>
2026-02-28 01:20:54 +0530

    Initial commit Notes Manager API with JWT Auth

---
+++ notes/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+
+
+from rest_framework import generics
+from .models import Note
+from .serializers import NoteSerializer
+from rest_framework.permissions import IsAuthenticated
+
+class NoteListCreate(generics.ListCreateAPIView):
+    serializer_class = NoteSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Note.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+class NoteDetail(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = NoteSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Note.objects.filter(user=self.request.user)
